7a1e340271fc30e888c4d84e4134911e82a11be6
Paul T. Grogan <pgrogan@stevens.edu>
2019-02-13 21:23:21 -0500

    fill out proxy outputs for orbit

--- orbits_proxy.py
+++ orbits_proxy.py
@@ -13,9 +13,17 @@ def execute(search_file, arch_dir, arch_file=None):
             arch = Architecture.from_json(arch_file)
     else:
         arch = Architecture.from_json(arch_file)
-    with open(os.path.join(arch_dir, 'lcl.csv'), 'w') as outfile:
+    with open(os.path.join(arch_dir, 'access.csv'), 'w', newline='') as outfile:
         writer = csv.writer(outfile)
-        writer.writerow(['test', 'test', 'test'])
+        writer.writerow(['Lat[deg]', 'Long[deg]', 'Access From [s]', 'Access To [s]'])
+    with open(os.path.join(arch_dir, 'gbl.csv'), 'w', newline='') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(["Time [s]", "", "TimeToCoverage [s]", "", "", "AccessTime [s]", "", "", "RevisitTime [s]", "", "", "Coverage", "NumOfPOIpasses", "", "", "Data Latency [s]", "", "", "NumGSpassesPD", "TotalDownlinkTimePD [s]", "DownlinkTimePerPass [s]", "", "", "CrossSwath[km]", "", "", "AlongSwath [km]", "", "", "SpatialResolution [m]", ""])
+        writer.writerow(["t0", "t1", "TCavg", "TCmin", "TCmax", "ATavg", "ATmin", "ATmax", "RTavg", "RTmin", "RTmax", "% Grid Covered", "PASavg", "PASmin", "PASmax", "DLavg", "DLmin", "DLmax", "PassesPerDay", "DLTimePerDay", "DLTavg", "DLTmin", "DLTmax", "CSavg", "CSmin", "CSmax", "ASavg", "ASmin", "ASmax", "SRmin", "SRmax"])
+    with open(os.path.join(arch_dir, 'lcl.csv'), 'w', newline='') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(["Time [s]", "", "POI", "[deg]", "[deg]", "[km]", "AccessTime [s]", "", "", "RevisitTime [s]", "", "", "TimeToCoverage [s]", "Number of Passes"])
+        writer.writerow(["t0", "t1", "POI", "lat", "lon", "alt", "ATavg", "ATmin", "ATmax", "RvTavg", "RvTmin", "RvTmax", "TCcov", "numPass"])
 
 class readable_dir(argparse.Action):
     """Defines a custom argparse Action to identify a readable directory."""
